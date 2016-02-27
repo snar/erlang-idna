ef8c8877b1e332550350947d3d652fa5c734c18a
Benoit Chesneau <bchesneau@gmail.com>
2016-02-27 09:39:24 +0100

    refactor and use one data file.

--- mkdata.sh
+++ mkdata.sh
@@ -17,39 +17,34 @@ fi
 
 DATA_FILE=UnicodeData.txt
 DATA_URL=http://www.unicode.org/Public/UNIDATA/UnicodeData.txt
-DATA1_OUT=src/idna_unicode_data1.erl
-DATA2_OUT=src/idna_unicode_data2.erl
+DATA_OUT=src/idna_unicode_data.erl
 
 # fetch data file
 $CURL_BIN -o $DATA_FILE $DATA_URL
 
 # create the first module
-cat <<EOF > $DATA1_OUT
--module(idna_unicode_data1).
+cat <<EOF > $DATA_OUT
+-module(idna_unicode_data).
+-export([lookup/1]).
 -export([decomposition/1]).
+-compile([compressed]).
 
 EOF
 cat $DATA_FILE \
     | awk 'BEGIN{FS=";"}{if($6!=""){ printf("decomposition(\"%s\") -> \"%s\";\n", $6, $1)}}' \
     | sort \
     | uniq -w 25 \
-    >> $DATA1_OUT
-echo "decomposition(_) -> false." >> $DATA1_OUT
-echo "" >> $DATA1_OUT
+    >> $DATA_OUT
+echo "decomposition(_) -> false." >> $DATA_OUT
+echo "" >> $DATA_OUT
 
 
-# create the second module
-cat <<EOF > $DATA2_OUT
--module(idna_unicode_data2).
--export([lookup1/1]).
-
-EOF
 cat $DATA_FILE \
-    | awk 'BEGIN{FS=";"}{if($1!=""){printf("lookup1(\"%s\") -> {\"%s\",\"%s\",\"%s\"};\n", $1, $4, $6, $14)}}' \
+    | awk 'BEGIN{FS=";"}{if($1!=""){printf("lookup(\"%s\") -> {\"%s\",\"%s\",\"%s\"};\n", $1, $4, $6, $14)}}' \
     | sort \
     | uniq -w 25 \
-	>> $DATA2_OUT
-echo "lookup1(_) -> false." >> $DATA2_OUT
-echo "" >> $DATA2_OUT
+	>> $DATA_OUT
+echo "lookup(_) -> false." >> $DATA_OUT
+echo "" >> $DATA_OUT
 
 
